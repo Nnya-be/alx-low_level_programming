5cb86c920b012d0f9b8c8186ea9430607d08c3e6
root <root@kali>
2023-07-14 08:40:50 -0400

    Made the quote file

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -5,7 +5,6 @@
  */
 int main(void)
 {
-char message[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
-fputs(message, stderr);
+fputs("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", stderr);
 return (1);
 }
