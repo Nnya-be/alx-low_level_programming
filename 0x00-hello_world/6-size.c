9ca2227bfc8ad9acfe512567ce226106d26b884c
root <root@kali>
2023-07-14 08:01:02 -0400

    Made the size.c file

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+ *main - Entry point
+ *Return: Always 0 (succes)
+ */
+int main(void)
+{
+printf("Size of char:"sizeof(char)"(s)");
+printf("Size of int:"sizeof(int)"(s)");
+printf("Size of long int:"sizeof(long int)"(s)");
+printf("Size of long long int:"sizeof(long long int)"(s)");
+printf("Size of float:"sizeof(float)"(s)");
+return (0);
+}
