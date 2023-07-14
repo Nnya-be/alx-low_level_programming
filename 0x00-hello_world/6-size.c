67a57185d60e9119dfa70d2c2ca7ae549e78fb31
root <root@kali>
2023-07-14 08:09:09 -0400

    Made the size.c file

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -5,10 +5,10 @@
  */
 int main(void)
 {
-printf("Size of char:"sizeof(char)"(s)");
-printf("Size of int:"sizeof(int)"(s)");
-printf("Size of long int:"sizeof(long int)"(s)");
-printf("Size of long long int:"sizeof(long long int)"(s)");
-printf("Size of float:"sizeof(float)"(s)");
+printf("Size of char:%d byte(s)\n", sizeof(char));
+printf("Size of int:%d byte(s)\n", sizeof(int));
+printf("Size of long int:%d byte(s)\n", sizeof(long int));
+printf("Size of long long int:%d byte(s)\n", sizeof(long long int));
+printf("Size of float:%d byte(s)\n", sizeof(float));
 return (0);
 }
