278735389b939b010c03428ae7b1406179e2710c
root <root@kali>
2023-07-17 16:15:15 -0400

    Modified the positive file

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -8,22 +8,15 @@
 
 int main(void)
 {
-int n;   /** Generating the seed for the random number*/
+int n;
 srand(time(0));
-  /** using the seed to generate the random number*/
 n = rand() - RAND_MAX / 2;
-  /** Comparing for the positive condition */
 if (n > 0)
-{
   printf("%d: is positive", n);
-}  /** Comparing for the negative condition */
 if (n < 0)
-{
   printf("%d: is negative", n);
-}  /** Comparing for the zero condition */
 if (n == 0)
-{
   printf("%d: is zero", n)
-}
+
 return (0);
 }
