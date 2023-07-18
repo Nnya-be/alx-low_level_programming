70ab9b4c7d33767806330489f316782638c03c0f
root <root@kali>
2023-07-18 00:05:59 -0400

    Made the alphabets  file

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+ *main - Entry point of program
+ *Return: Always 0 (success)
+ */
+int main(void)
+{
+for (char c = 'a' ; c <= 'z' ; c++)
+{
+putchar(c);
+}
+putchar("\n");
+return (0);
+}
