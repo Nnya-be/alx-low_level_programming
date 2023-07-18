a08bf29a3d0a6f12a2f1fa6bdad6589f03277d94
root <root@kali>
2023-07-18 01:07:46 -0400

    file

--- 0x01-variables_if_else_while/5-print_numbers.c
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -5,10 +5,12 @@
  */
 int main(void)
 {
-for (int i = 0; i < 10; i++)
-{
-printf("%d", i);
-}
-putchar('\n');
-return (0);
+int i;
+
+	for (i = 0; i <= 9; i++)
+	{
+		printf("%d", i);
+	}
+	putchar('\n');
+	return (0);
 }
