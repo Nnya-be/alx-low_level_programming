16e2f43a029086ac50f01632e20f5a535b604810
root <root@kali>
2023-07-18 01:11:06 -0400

    file

--- 0x01-variables_if_else_while/6-print_numberz.c
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -6,10 +6,11 @@
 int main(void)
 {
 int i;
-for (i = '0'; i < '10'; i++)
-{
-putchar(i);
-}
-putchar('\n');
-return (0);
+
+	for (i = '0'; i <= '9'; i++)
+	{
+		putchar(i);
+	}
+	putchar('\n');
+	return (0);
 }
