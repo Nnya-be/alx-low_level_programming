ace8204a22ed41c4feb7082c62755c0fbc360d3e
root <root@kali>
2023-07-19 23:56:42 -0400

    changes made to most files

---
+++ 0x02-functions_nested_loops/101-natural.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ *print_multiple - takes numbers that are multiples of 3 and 5
+ */
+void print_multiple(void)
+{
+int number;
+int added;
+for (number = 1023; number > 0; number --)
+{
+if((number % 3) == 0)
+added += number;
+else if ((number % 5) == 0)
+added += number;
+}
+printf("%d\n", added);
+}
