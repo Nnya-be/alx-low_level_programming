f2c26394cd0cb690bcb92d8a40ddd1b61c5d4254
root <root@kali>
2023-07-20 00:25:40 -0400

    changes made to most files

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -10,9 +10,11 @@ for (a = 0; a < 24; a++)
 {
 for (b = 0; b < 60; b++)
 {
-_putchar('0' + (a / 10) + (a % 10));
+_putchar((a / 10) + '0');
+ _putchar((a % 10) + '0');
 _putchar(':');
-_putchar('0' + (b / 10) + (b % 10));
+ _putchar('0' + (b / 10));
+ _putchar((b % 10) + '0');
 _putchar('\n');
 }
 }
--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -12,9 +12,24 @@ for (i = 0; i < 10; i++)
 for (j = 0; j < 10; j++)
 {
 num = i * j;
+if (j == 0)
+{
 _putchar(num + '0');
+}
+if (num < 10 && j != 0)
+{
 _putchar(',');
 _putchar(' ');
+_putchar(' ');
+_putchar(num + '0');
+}
+else if ( num >= 10)
+{
+_putchar(',');
+_putchar(' ');
+_putchar((num / 10) + '0');
+_putchar((num % 10) + '0');
+}
 }
 _putchar('\n');
 }
