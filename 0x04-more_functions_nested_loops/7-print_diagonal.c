c23f339440dbb83c5b3129be9c3b25e10bc27f87
root <root@kali>
2023-07-20 18:33:47 -0400

    Made the print diagonal file

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -8,10 +8,10 @@ void print_diagonal(int n)
 if (n > 0)
 {
 int i;
-for (i = 0; i <= n; i++)
-{
 int j;
-for (j = 0; j <= i; j++)
+for (i = 1; i <= n; i++)
+{
+for (j = 1; j <= i; j++)
 {
 if (j == i)
 _putchar('\\');
