267163902342ad09c0d5865d7ad2df5330ce6f9a
root <root@kali>
2023-07-19 23:35:15 -0400

    changes made to most files

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -5,20 +5,24 @@
  */
 void print_to_98(int n)
 {
-if (n < 99)
+int i, j;
+if (n <= 98)
 {
-while (n < 99)
+for (i = n; i <= 98; i++)
 {
-_putchar(n);
-n++;
+if (i != 98)
+printf("%d,", i);
+else if (i == 98)
+printf("%d\n", i);
 }
 }
-else
+else if (n >= 98)
 {
-while (n > 97)
+for (j = n; j >= 98, j--)
 {
-_putchar(n);
-n--;
+if (j != 98)
+printf("%d,", j);
+else if ("%d\n", j)
 }
 }
 }
