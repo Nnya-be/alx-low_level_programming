09568c27b1a451a95072c7786e16cdcaadfe881f
root <root@kali>
2023-07-20 16:11:38 -0400

    Made the isdigit file

--- 0x04-more_functions_nested_loops/1-isdigit.c
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -1,8 +1,8 @@
 #include "main.h"
 /**
  *_isdigit - checks to see if a number is a digit.
- *@c - the number to be checked
- *Return - 0 on false 1 on true.
+ * @c : type int the number to be checked
+ *Return: 0 Always on false 1 on true.
  */
 int _isdigit(int c)
 {
