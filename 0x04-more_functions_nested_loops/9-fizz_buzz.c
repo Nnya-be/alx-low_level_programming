5dbccbc11e382540e9408c65a64799e3802e7c3e
root <root@kali>
2023-07-20 19:02:37 -0400

    Made the fizz buzz file

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -5,16 +5,16 @@
 void fizz_buzz(void)
 {
 int i;
-char fizz[] = "fizz";
-char buzz[] = "buzz";
-char fizz_b[] = "fizzbuzz";
+char fizz[] = "Fizz";
+char buzz[] = "Buzz";
+char fizz_b[] = "FizzBuzz";
 for (i = 1; i <= 100; i++)
 {
 if ((i % 3) == 0)
 printf("%s", fizz);
 else if ((i % 5) == 0)
 printf("%s", buzz);
-else if ((i % 3) == 0 && (i % 5) == 0)
+else if (((i % 3) == 0) && ((i % 5) == 0))
 printf("%s", fizz_b);
 else
 printf("%d", i);
