dabde5eaafd7b900d4ca3575d8e1c582c1bbf1ac
Solomon <solomonchamamme@gamil.com>
2023-07-24 19:25:58 -0400

    Made the strlen file

--- 0x05-pointers_arrays_strings/2-strlen.c
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -4,11 +4,11 @@
  *@s : the string to be passed in
  *Return: length of string
  */
+int length;
 int _strlen(char *s)
 {
-int length;
 int i;
-while (*(s + i) != '\0')
+while (s[i] != '\0')
 {
 length++;
 i++;
