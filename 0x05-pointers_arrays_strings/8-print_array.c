c33b833747791c444e49570b0cd21c46e9aed889
Solomon <solomonchamamme@gamil.com>
2023-07-25 07:32:43 -0400

    made a file

---
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <string.h>
+#include "main.h"
+/**
+ *print_array - prints the content of an array
+ *@a : the array
+ *@n : the number contained in the array
+ */
+void print_array(int *a, int n)
+{
+int i;
+for (i = 0; i < n; i++)
+{
+printf("%d,", a[i]);
+}
+_putchar('\n');
+}
