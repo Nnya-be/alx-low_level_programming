ba5beaf32b55446744b0d367d8f46b636661602a
Solomon <solomonchamamme@gamil.com>
2023-07-25 07:10:15 -0400

    made a file

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,24 @@
+#include <string.h>
+#include "main.h"
+/**
+ *puts_half - prints half of string
+ *@str : the string to be manipulated
+ *return: nothing
+ */
+void puts_half(char *str)
+{
+int length = strlen(str);
+int i;
+if (length % 2 == 0)
+{
+for (i = length / 2; i < length; i++)
+{
+_putchar(str[i]);
+}
+return;
+}
+for (i = (length - 1) / 2; i < length; i++)
+{
+_putchar(str[i]);
+}
+}
