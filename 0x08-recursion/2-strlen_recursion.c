0989c53da3a1c0526b4ba0db022c869da4edde49
root <root@kali>
2023-08-09 14:40:23 -0400

    Created a new file

--- 0x08-recursion/2-strlen_recursion.c
+++ 0x08-recursion/2-strlen_recursion.c
@@ -6,14 +6,12 @@
  */
 int _strlen_recursion(char *s)
 {
-int number;
 if (*s == '\0')
 {
-return (number);
+return (0);
 }
 else
 {
-number++;
-_strlen_recursion(s + 1);
+return (1 + _strlen_recursion(s + 1));
 }
 }
