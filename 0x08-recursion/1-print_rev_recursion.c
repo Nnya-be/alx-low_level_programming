f00d313b333bbd0207ae02295a7df703f8a61ec5
root <root@kali>
2023-08-09 14:06:08 -0400

    Created a new file

---
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+*_print_rev_recursion - prints a string in reverse
+*Return : nothing on success
+*@s : the string to be used
+*/
+void _print_rev_recursion(char *s)
+{
+if (*s != '\0')
+{
+_putchar(*s);
+_print_rev_recursion(s - 1);
+}
+_putcahr('\n');
+return;
+}
