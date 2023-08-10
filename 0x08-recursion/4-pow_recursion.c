5528b102c5269abbe0ba21a1b007647c68e3e1dd
root <root@kali>
2023-08-10 11:52:49 -0400

    Created a new file

---
+++ 0x08-recursion/4-pow_recursion.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ *_pow_recursion - takes the power of a number to another
+ *@x : the first number
+ *@y : the second number
+ *Return: negative on less than 0
+ */
+int _pow_recursion(int x, int y)
+{
+if (y < 0)
+{
+return (-1);
+}
+return (x * _pow_recursion(x, y - 1));
+}
