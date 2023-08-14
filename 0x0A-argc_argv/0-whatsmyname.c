95561bc2eccdb393a0105ba565c08db3de31e272
root <root@kali>
2023-08-14 02:10:54 -0400

    made changes to the file

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -3,9 +3,9 @@
  *main - the entry point of the program
  *@argv : the arg vector
  *@argc : the arg counter
- *Return : always 0
+ *Return: 0 on success
  */
-int main (int argc , char *argv[])
+int main(int argc, char *argv[])
 {
 printf("%s", argv[0]);
 return (0);
