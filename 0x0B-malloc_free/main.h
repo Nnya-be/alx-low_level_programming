cee1876c9003114916d8ceac1f3376ce81e6adaa
root <root@kali>
2023-08-14 02:54:44 -0400

    Created a new file

--- 0x0B-malloc_free/main.h
+++ 0x0B-malloc_free/main.h
@@ -2,4 +2,10 @@
 #define MIAN_H_
 int _putchar(char);
 char *create_array(unsigned int, char);
+char *_strdup(char *);
+char *str_concat(char *, char *);
+int **alloc_grid(int, int);
+void free_grid(int **, int);
+char *argstostr(int, char **);
+char **strtow(char *);
 #endif
