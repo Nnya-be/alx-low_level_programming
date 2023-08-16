2ad04cddea3321713a73e66a15fce06e6f1ac1b4
root <root@kali>
2023-08-16 19:02:49 -0400

    made the array iterator

---
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -0,0 +1,15 @@
+#include "function_pointers.h"
+/**
+ *array_iterator - function that prints the array using a function pointer
+ *@array : the array to be printed
+ *@size : size of the array
+ *@action: the function pointer
+ */
+void array_iterator(int *array, size_t size, void (*action)(int))
+{
+int i;
+for (i = 0; i < size; i++)
+{
+action(array[i]);
+}
+}
--- 0x0F-function_pointers/function_pointers.h
+++ 0x0F-function_pointers/function_pointers.h
@@ -2,4 +2,5 @@
 #define FUNCTION_POINTERS_H
 int _putchar(char);
 void print_name(char *, void (*f)(char *));
+void array_iterator(int *, size_t, void (*action)(int));
 #endif
