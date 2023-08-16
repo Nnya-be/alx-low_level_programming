25d459f366cf9891f051c2cd4eafa0505b96e0c4
root <root@kali>
2023-08-16 19:07:58 -0400

    modified

--- 0x0F-function_pointers/function_pointers.h
+++ 0x0F-function_pointers/function_pointers.h
@@ -2,5 +2,5 @@
 #define FUNCTION_POINTERS_H
 int _putchar(char);
 void print_name(char *, void (*f)(char *));
-void array_iterator(int *, size_t, void (*action)(int));
+void array_iterator(int *array, size_t size, void (*action)(int));
 #endif
