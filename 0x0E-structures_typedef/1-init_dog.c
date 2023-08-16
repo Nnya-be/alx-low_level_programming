1d29373408df16ff2808252e04fe6360a13ebce6
root <root@kali>
2023-08-15 22:40:49 -0400

    Created the dog init file

---
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ *init_dog - Function to initialize the struct
+ *@d : the first argument
+ *@name: the second argument
+ *@age : the third argument
+ *@owner: the fourth argument
+ */
+void init_dog(struct dog *d, char *name, float age, char *owner)
+{
+d.name = name;
+d.age = age;
+d.owner = owner;
+}
