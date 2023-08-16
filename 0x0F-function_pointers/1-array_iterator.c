da8bcf360a27ec75a3855210e4a027c322b572ed
root <root@kali>
2023-08-16 19:19:35 -0400

    new commit

--- 0x0F-function_pointers/1-array_iterator.c
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -5,7 +5,7 @@
  *@size : size of the array
  *@action: the function pointer
  */
-void array_iterator(int *array, size_t size, void (*action)(int))
+void array_iterator(int *array, size_t size, void (*action)(int)
 {
 size_t i;
 for (i = 0; i < size; i++)
