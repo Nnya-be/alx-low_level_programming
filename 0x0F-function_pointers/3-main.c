17f2c801551ab942cd49e3e054d441b0898f9044
root <root@kali>
2023-08-17 07:56:31 -0400

    changes

--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -24,7 +24,7 @@ if (f == NULL)
 printf("Error\n");
 return (99);
 }
-if ((*(argv[2]) == '/' || *(argv[2]) == '%') && b == 0 || argv[2][1] != '\0')
+if (((*(argv[2]) == '/' || *(argv[2]) == '%') && b == 0) || argv[2][1] != '\0')
 {
 printf("Error\n");
 return (100);
