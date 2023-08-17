d346e89402e5002855d2e93f878ca3f2375fbbec
root <root@kali>
2023-08-17 09:24:25 -0400

    changes made to files

--- 0x0F-function_pointers/100-main_opcodes.c
+++ 0x0F-function_pointers/100-main_opcodes.c
@@ -8,27 +8,29 @@
  */
 int main(int argc, char *argv[])
 {
-int b = atoi(argv[1]);
+int b;
 int i;
-unsigned char *ptr = (unsigned char *)main;
+char *ptr = (unsigned char *)main;
 if (argc != 2)
 {
 printf("Error\n");
 exit(1);
 }
+b = atoi(argv[1]);
 if (b <= 0)
 {
 printf("Error\n");
 exit(2);
 }
+ptr = (char *)main;
 for (i = 0; i < b; i++)
 {
-if (i != b - 1)
+if (i == b - 1)
 {
-printf(" ");
+printf("%.2hhx\n", ptr[i]);
+break;
 }
-printf("%.2hhx", *(ptr + 1));
+printf("%.2hhx", ptr[i]);
 }
-printf("\n");
 return (0);
 }
