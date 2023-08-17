aa6dfaff353b74628c1f6c329b22cd92cd76d5c5
root <root@kali>
2023-08-17 01:43:28 -0400

    Changes made to the int index file

--- 0x0F-function_pointers/2-int_index.c
+++ 0x0F-function_pointers/2-int_index.c
@@ -10,10 +10,11 @@ int int_index(int *array, int size, int (*cmp)(int))
 {
 int i;
 int number;
-if (size <= 0)
+if (size <= 0 || array == NULL || cmp == NULL)
 {
 return (-1);
 }
+
 for (i = 0; i < size; i++)
 {
 number = cmp(array[i]);
