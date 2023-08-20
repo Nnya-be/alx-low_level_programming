79805c9fd64de44ca87d304e3885bda6487af58c
root <root@kali>
2023-08-19 21:41:07 -0400

    Changes made to the print dog file

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -6,8 +6,10 @@
  */
 void print_dog(struct dog *d)
 {
-if (d != NULL)
+if (d == NULL)
 {
+return;
+}
 if (d->name != NULL)
 {
 printf("Name: %s\n", d->name);
@@ -31,6 +33,5 @@ printf("Owner: %s\n", d->owner);
 else
 {
 printf("Owner: (nil)\n");
-}
 }
 }
