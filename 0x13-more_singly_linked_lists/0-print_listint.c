1a9ef5d953ab442b94913aac3bb462b6029b98bb
Solomon <solomonchamamme@gmail.com>
2023-08-30 02:15:04 +0000

    modified

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -2,7 +2,7 @@
 /**
 *print_listint - Function to print the elements in the linked lists
 *@h: the head of the linked lists
-*Return : The linked lists
+*Return: Number of lists
 */
 
 size_t print_listint(const listint_t *h)
