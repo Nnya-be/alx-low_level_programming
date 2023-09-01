ac3e930e3cb43b61a1f06c96813c0c55eaccf781
Solomon <solomonchamamme@gmail.com>
2023-09-01 20:33:39 +0000

    Created the clear bit file

---
+++ 0x14-bit_manipulation/4-clear_bit.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ *clear_bit - used to clear a bit at an index
+ *@n: a pointer to the number
+ *@index: the index to work on
+ *Return: The number
+ */
+int clear_bit(unsigned long int *n, unsigned int index)
+{
+unsigned long int i;
+if (index >= size(unsigned long int) * 8)
+{
+return (-1);
+}
+i = 1UL << index;
+i = ~i;
+*n &= i;
+return (1);
+}
