25432e9d0c9bf1983368a212dc6356c77712b050
Solomon <solomonchamamme@gmail.com>
2023-09-01 20:41:38 +0000

    Made changes:

--- 0x14-bit_manipulation/4-clear_bit.c
+++ 0x14-bit_manipulation/4-clear_bit.c
@@ -8,7 +8,7 @@
 int clear_bit(unsigned long int *n, unsigned int index)
 {
 unsigned long int i;
-if (index >= size(unsigned long int) * 8)
+if (index >= sizeof(unsigned long int) * 8)
 {
 return (-1);
 }
