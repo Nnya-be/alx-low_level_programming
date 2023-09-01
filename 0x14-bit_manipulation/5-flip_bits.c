fe8969717d88062e2db9c26f7de6e506a29f5251
Solomon <solomonchamamme@gmail.com>
2023-09-01 20:47:23 +0000

    Made the flip bit

---
+++ 0x14-bit_manipulation/5-flip_bits.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ *flip_bits - Flip the bits at an index
+ *@n : The number
+ *@m : The replacing number
+ *Return: The modified number
+ */
+unsigned int flip_bits(unsigned long int n, unsigned long int m)
+{
+unsigned long int result = n ^ m;
+unsigned int count = 0;
+while (result > 0)
+{
+count += return & 1;
+result >>= 1;
+}
+return (count);
+}
