fe8b7950c6817c03d76b2da705e39289d9bf82f1
Solomon <solomonchamamme@gmail.com>
2023-09-06 21:19:51 +0000

    Another changes

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -1,61 +1,64 @@
 #include "main.h"
 /**
-*main - Copy contents to other files
-*@ac: arg count
-*@av: arg vector
-*Return: 0 on success
-*/
-int main(int ac, char **av)
+ * error_file - checks if files can be opened.
+ * @file_from: file_from.
+ * @file_to: file_to.
+ * @argv: arguments vector.
+ * Return: no return.
+ */
+void error_file(int file_from, int file_to, char *argv[])
 {
-int src, dst, bytes_rd, bytes_wr;
-char buffer[BUFFSIZE];
-if (ac != 3)
+if (file_from == -1)
 {
-print_exit(97, "Usage: cp file_from file_to\n", av[0]);
+dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
+exit(98);
 }
-src = open(av[1], O_RDONLY);
-if (src == -1)
+if (file_to == -1)
 {
-print_exit(98, "Error: Can't read from file %s\n", av[1]);
-}
-dst = open(av[2], O_WRONLY | O_CREAT | O_TRUNC, 0664);
-if (dst == -1)
-{
-print_exit(99, "Error: Can't wirte to %s\n", av[2]);
-}
-while ((bytes_rd = read(src, buffer, BUFFSIZE)) > 0)
-{
-bytes_wr = write(dst, buffer, bytes_rd);
-if (bytes_wr == -1)
-{
-print_exit(99, "Error: Can't write to %s\n", av[2]);
+dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
+exit(99);
 }
 }
-if (bytes_rd == -1)
+/**
+ * main - where execution starts
+ * @argc: number of args
+ * @argv: args vector
+ * Return: Always 0
+ */
+int main(int argc, char *argv[])
 {
-print_exit(98, "Error: Can't read from file %s\n", av[1]);
+int file_from, file_to, err_close;
+ssize_t nchars, nwr;
+char buf[1024];
+if (argc != 3)
+{
+dprintf(STDERR_FILENO, "%s\n", "Usage: cp file_from file_to");
+exit(97);
 }
-if (close(src) == -1)
+file_from = open(argv[1], O_RDONLY);
+file_to = open(argv[2], O_CREAT | O_WRONLY | O_TRUNC | O_APPEND, 0664);
+error_file(file_from, file_to, argv);
+nchars = 1024;
+while (nchars == 1024)
 {
-dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", src);
+nchars = read(file_from, buf, 1024);
+if (nchars == -1)
+error_file(-1, 0, argv);
+nwr = write(file_to, buf, nchars);
+if (nwr == -1)
+error_file(0, -1, argv);
+}
+err_close = close(file_from);
+if (err_close == -1)
+{
+dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", file_from);
 exit(100);
 }
-if (close(dst) == -1)
+err_close = close(file_to);
+if (err_close == -1)
 {
-dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", dst);
+dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", file_from);
 exit(100);
 }
 return (0);
-}
-
-/**
-*print_exit - print an error with exit code
-*@code: exit code
-*@msg: Message of error
-*@file: file name
-*/
-void print_exit(int code, const char *msg, const char *file)
-{
-dprintf(STDERR_FILENO, msg, file);
-exit(code);
 }
