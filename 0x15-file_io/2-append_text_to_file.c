729b9a52373d9ae8469bf29efa54f6349c023122
Solomon <solomonchamamme@gmail.com>
2023-09-06 20:55:13 +0000

    Corrections to edge cases

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -9,7 +9,7 @@ int append_text_to_file(const char *filename, char *text_content)
 {
 int fd;
 ssize_t b_written;
-if (text_content == NULL || filename == NULL)
+if (filename == NULL)
 {
 return (-1);
 }
