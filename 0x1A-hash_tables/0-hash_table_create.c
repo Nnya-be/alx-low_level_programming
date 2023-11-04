5d9462e9c9ad4d532fa665d02f1cb24a475376f3
Solomon Chamamme Nnyabe <solomonchamamme@gmail.com>
2023-11-04 22:40:36 +0000

    Modified: task 0

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -6,24 +6,24 @@
 */
 hash_table_t *hash_table_create(unsigned long int size)
 {
-size_t i = 0;
-hash_table_t *new_table = malloc(sizeof(hash_table_t));
+	size_t i = 0;
+	hash_table_t *new_table = malloc(sizeof(hash_table_t));
 
-if (new_table == NULL)
-{
-return (NULL);
-}
-new_table->size = size;
-new_table->array = malloc(sizeof(hash_node_t *) * size);
-if (new_table->array == NULL)
-{
-free(new_table);
-return (NULL);
-}
-while (i < size)
-{
-new_table->array[i] = NULL;
-i++;
-}
-return (new_table);
+	if (new_table == NULL)
+	{
+		return (NULL);
+	}
+	new_table->size = size;
+	new_table->array = malloc(sizeof(hash_node_t *) * size);
+	if (new_table->array == NULL)
+	{
+		free(new_table);
+		return (NULL);
+	}
+	while (i < size)
+	{
+		new_table->array[i] = NULL;
+		i++;
+	}
+	return (new_table);
 }
