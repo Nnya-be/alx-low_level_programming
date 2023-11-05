b8397bea769fe59759f2b3daf04459caf5f33050
Solomon Chamamme Nnyabe <solomonchamamme@gmail.com>
2023-11-05 01:01:46 +0000

    modified

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -13,18 +13,24 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 	size_t index;
 
 	if (key == NULL || ht == NULL || *key == '\0' || value == NULL)
-	{
 		return (0);
-	}
-	if (new_node == NULL)
-	{
-		return (0);
-	}
 	index = key_index((const unsigned char *)key, ht->size);
+	temp_node = ht->array[index];
+	while (temp_node != NULL)
+	{
+		if (strcmp(temp_node->key, key) == 0)
+		{
+			free(temp_node->value);
+			temp_node->value = strdup(value);
+			if (temp_node->value == NULL)
+				return (0);
+			return (1);
+		}
+		temp_node = temp_node->next;
+	}
 	new_node->key = strdup(key);
 	if (new_node->key == NULL)
-	{
-		free(new_node);
+	{	free(new_node);
 		return (0);
 	}
 	new_node->value = strdup(value);
@@ -40,7 +46,5 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 	}
 	new_node->next = ht->array[index];
 	ht->array[index] = new_node;
-	free(new_node);
-	free(temp_node);
 	return (1);
 }
