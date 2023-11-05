a205fe65ca10a829db9ebc19def3c8f3762b5fa6
Solomon Chamamme Nnyabe <solomonchamamme@gmail.com>
2023-11-05 16:55:53 +0000

    Added: print table

---
+++ 0x1A-hash_tables/5-hash_table_print.c
@@ -0,0 +1,34 @@
+#include "hash_tables.h"
+/**
+ *hash_table_print - A function that prints a hash table.
+ *@ht: The hash table to print.
+ */
+
+void hash_table_print(const hash_table_t *ht)
+{
+	hash_node_t *array_node = malloc(sizeof(hash_node_t));
+	unsigned long int i = 0;
+	int first_node = 1;
+	
+	printf("{");
+	if (ht == NULL)
+	{
+		printf("}\n");
+		return;
+	}
+	while (i < ht->size)
+	{
+		array_node = ht->array[i];
+		while (array_node != NULL)
+		{
+			if (!first_node)
+				printf(", ");
+			printf("\'%s\': \'%s\'", array_node->key, array_node->value);
+			first_node = 0;
+			array_node = array_node->next;
+		}
+	
+		i++;
+	}
+	printf("}\n");
+}
