22a7ad4d8ef635ee8624fabd0475d0657acd1368
Solomon Chamamme Nnyabe <solomonchamamme@gmail.com>
2024-02-12 22:51:33 +0000

    Modified: the function prototypes

--- 0x1E-search_algorithms/search_algos.h
+++ 0x1E-search_algorithms/search_algos.h
@@ -8,6 +8,6 @@
 
 /** Function Definitions*/
 int linear_search(int *array, size_t size, int value);
-
+int binary_search(int *array, size_t size, int value);
 /** Ending the function definitions */
 #endif
